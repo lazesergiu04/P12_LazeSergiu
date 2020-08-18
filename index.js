9e7a5aa974d9e9f2e24801f38e74a1fe8b7a892b
lazesergiu <lazesergiu04@gmail.com>
2020-08-18 23:27:40 +0100

    Param solved

--- index.js
+++ index.js
@@ -12,6 +12,7 @@ app.use(express.static(path.join('www', 'build')));
 app.use(bodyParser.json());
 
 const { Client } = require('pg');
+const e = require('express');
 
 const connectionStr= process.env.DATABASE_URL || 'postgres://expfftnffbdkvh:8afc4a8c06c78c97f53b2e25b8a0581bd6c8fdd7356a0fd06ebc2b3b91912ad8@ec2-18-210-214-86.compute-1.amazonaws.com:5432/d4v5pjn3g2jgag';
 
@@ -24,48 +25,52 @@ const client = new Client({
 });
 
 client.connect();
-var contactEmails =[];
-var contactIds =[];
-var contactSfIds =[];
-var contacts= [];
 
 app.get('/contacts', (req, res)=>{
-  client.query(`Select id, sfid, email from salesforce.Contact`, (err, data)=>{
+  client.query(`Select * from salesforce.Contact`, (err, data)=>{
     res.json(data);
   })
 })
 
 //Get the Contact records ( email, id, sfId)
 
-app.get('/contacts/:email', (req, res)=>{
-  client.query(`Select sfid from salesforce.Contacts Where Email= ${1}`,[req.body.email], (err, data)=>{
-    if(data.rowCount == 0){
-      app.put(`/`,(req, res)=>{
-        client.query(`Insert Into salesforce.Contact (FirstName, LastName, Email) Values($1,$2, $3, $4)`,
-        [req.body.firstName.trim(), req.body.lastName.trim(), req.body.email.trim()],(err, data)=>{
-          res.json(data.rows[0].sfid);
-
-        })
-      } )
-
-
+app.get('/contact', (req, res)=>{
+  console.log(req.param('email'));
+  var email = req.param('email');
+  var lastname =req.param('lastname');
+  
+  client.query(`Select sfid from salesforce.Contact Where email='${email}'`, (err, data)=>{
+    if(data !== undefined){
+      if(data.rowCount == 0){ 
+         console.log('Create record executed --');
+      app.post(`/contact`,(req, res)=>{
+          client.query(`Insert Into salesforce.Contact (LastName, Email) 
+          Values('${lastname}', '${email}')`,(err, data)=>{
+            console.log('test');
+            res.json(data.rows[0].sfid);
+          })
+        } )
+      }else{
+        res.json(data.rows[0].sfid);
+      }  
     }else{
-      res.json(data.rows[0].sfid);
+      console.log(' data undefined');
     }
+     
   })
     });
 
 
 
     //Update the modified contacts
-    app.patch('/contacts/:sfid', (req, res)=>{
-      client.query(`Update  salesforce.Contact where Sfid = ${1}`, [req.body.sfid],(err,data)=>{
+    app.patch('/contacts', (req, res)=>{
+      client.query(`Update  salesforce.Contact where Sfid = '${1}'`, [req.param('sfid')],(err,data)=>{
         res.json(data.rows[0]);
       })
 
     })
 
-    //Dectivate in Salesforce the deteleted contacts
+    //Dectivate in Salesforce the deteleted contacts??
     //I created a checkbox IsActive that will be assign to false
     app.patch('/contacts/:sfid',(req, res)=>{
       client.query(`Update salesforce.Contact  Set IsActive__c=${1}`,[false], (err, data)=>{
